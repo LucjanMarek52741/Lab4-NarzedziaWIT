3e40d022a4e1d0dfa238365ac68032940662e7c3
LucjanMarek52741 <dsw52741@student.dsw.edu.pl>
2024-07-06 21:30:27 +0200

    Dodano --logs

--- skrypt.sh
+++ skrypt.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+create_logs() {
+  local count=$1
+  for ((i=1; i<=count; i++)); do
+    echo -e "Nazwa pliku: log$i.txt\nNazwa skryptu: skrypt.sh\nData: $(date)" > log$i.txt
+  done
+}
+
 case "$1" in
   --date)
     date
+    ;;
+  --logs)
+    count=${2:-100}
+    create_logs $count
     ;;
   *)
     echo "Nieznana opcja: $1"
